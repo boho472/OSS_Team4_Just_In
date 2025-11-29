9de1fbeb96a8c72eec6522b8294a1536cd4bee3a
Jay Lee <juno7014@naver.com>
2025-11-30 00:28:34 +0900

    build_sam 수정

--- HybridTrack_modify2/src/model/tracking_wrapper_mot.py
+++ HybridTrack_modify2/src/model/tracking_wrapper_mot.py
@@ -32,32 +32,27 @@ def _load_checkpoint(model, ckpt_path):
 
 
 def build_sam(config_file, ckpt_path=None, device="cuda", mode="eval", hydra_overrides_extra=[], apply_postprocessing=True):
-    # hydra_overrides = [
-    #     "++model._target_=sam2.sam2_video_predictor.SAM2VideoPredictor",
-    # ]
+
     hydra_overrides = []
     if apply_postprocessing:
         hydra_overrides_extra = hydra_overrides_extra.copy()
         hydra_overrides_extra += [
-            # dynamically fall back to multi-mask if the single mask is not stable
             "++model.sam_mask_decoder_extra_args.dynamic_multimask_via_stability=true",
             "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_delta=0.05",
             "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_thresh=0.98",
-            # the sigmoid mask logits on interacted frames with clicks in the memory encoder so that the encoded masks are exactly as what users see from clicking
             "++model.binarize_mask_from_pts_for_mem_enc=true",
-            # fill small holes in the low-res masks up to `fill_hole_area` (before resizing them to the original video resolution)
-            # "++model.fill_hole_area=8",  # [AL] commented out due to different SAM class
         ]
     hydra_overrides.extend(hydra_overrides_extra)
 
-    # Read config and init model
-    cfg = compose(config_name=config_file, overrides=hydra_overrides)
-    OmegaConf.resolve(cfg)
-    model = instantiate(cfg.model, _recursive_=True)
-    _load_checkpoint(model, ckpt_path)
-    model = model.to(device)
-    if mode == "eval":
-        model.eval()
+    # Hydra initialize
+    with initialize(config_path="../../configs"):  # test_integration1.py 기준
+        cfg = compose(config_name="sam2.1.sam21pp_hiera_t", overrides=hydra_overrides)
+        OmegaConf.resolve(cfg)
+        model = instantiate(cfg.model, _recursive_=True)
+        _load_checkpoint(model, ckpt_path)
+        model = model.to(device)
+        if mode == "eval":
+            model.eval()
     return model
 
 
